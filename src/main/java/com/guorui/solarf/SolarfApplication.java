dfa669dc03cd3cedc236cd8333cf7e40a9ae44fb
GuoRui111 <a2363478729@163.com>
2019-10-20 15:49:36 +0800

    first commit

---
+++ src/main/java/com/guorui/solarf/SolarfApplication.java
@@ -0,0 +1,13 @@
+package com.guorui.solarf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolarfApplication.class, args);
+    }
+
+}
